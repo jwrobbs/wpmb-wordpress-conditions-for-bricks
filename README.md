19e27cdedfb8fd9657ec836250f9264b10784821
Josh Robbs <josh@joshrobbs.com>
2024-10-15 10:44:13 -0400

    Add .gitignore, README.md, composer.json, and wordpress-conditions-for-bricks.php

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # wpmb-wordpress-conditions-for-bricks
 Custom Bricks conditions based on WordPress conditions like is_home (WordPress plugin)
+
+# Status
+In development - not safe to use
+
+
---
+++ wordpress-conditions-for-bricks.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: WPMB WordPress Conditions for Bricks
+ * Plugin URI: https://wpmasterbuilder.com
+ * Description: Custom Bricks conditions based on WordPress conditions like is_home (WordPress plugin).
+ * Version: 0.1.0
+ * Author: Josh Robbs
+ * Author URI: https://wpmasterbuilder.com
+ * License: GPL2
+ *
+ * @package WPMB_WordPress_Conditions_for_Bricks
+ */
+
+namespace WPMB_WordPress_Conditions_for_Bricks;
+
+defined( 'ABSPATH' ) || exit;
